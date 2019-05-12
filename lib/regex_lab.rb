e01fbdb05750d34f93417bd4ea29fb588fed531f
elijahmm17 <elijahmm17@gmail.com>
2019-05-12 05:20:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  word = text.match()
+  word = text.match(/)
+  if word.size > 0 
+    return true 
+  else 
+    return false 
+  end
 end
 
 def valid_phone_number?(phone)
