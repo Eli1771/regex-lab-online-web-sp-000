582dc8bd792f4ad91c79a83f257ff6227d23fb0a
elijahmm17 <elijahmm17@gmail.com>
2019-05-12 05:16:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,20 @@
 def starts_with_a_vowel?(word)
-
+   list = word.scan(/^[aeiouAEIOU]/)
+   if list.size > 0 
+     return true 
+   else 
+     return false 
+   end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  array = text.scan(/\bun\w*ing\b/)
+  return array
 end
 
 def words_five_letters_long(text)
-
+  array = text.scan(/\b\w{5}\b/)
+  return array
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
